5f3ea7847c149b927625c16a567d549ccae5a45b
Camille <78221213+clatapie@users.noreply.github.com>
2023-05-24 17:57:07 +0200

    Fixing `_read_config_file` (#42)
    * Fixing `_read_config_file`
    
    * Removing typos

--- tests/test_path.py
+++ tests/test_path.py
@@ -50,7 +50,7 @@ def test_migration():
     _clear_config_file()
 
     old_settings_dir = appdirs.user_data_dir(f"ansys_mapdl_core")
-    os.makedirs(old_settings_dir)
+    os.makedirs(old_settings_dir, exist_ok=True)
     old_config_file = os.path.join(old_settings_dir, "config.txt")
     shell = r"C:\Windows\System32\cmd.exe" if os.name == "nt" else "/bin/bash"
     with open(old_config_file, "w") as f:
