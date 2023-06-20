44bd275d597478f5de5f58222750e21da610ce4e
Jamil Hajjar <hajjarjamil4@gmail.com>
2023-06-20 12:06:11 +0200

    fix is_valid_executable_path() call for mechanical (#55)
    * fix error
    
    * simplify _is_common_executable_path
      * add back the version check and simplify _is_common_executable_path without any if
      * add tests for _is_common_executable
      * remove useless if statement since it is always True
      * try displaying v_version and ansys_version
      * fix _is_common_executable_path tests
    
    * add some tests

--- src/ansys/tools/path/path.py
+++ src/ansys/tools/path/path.py
@@ -64,7 +64,9 @@ if not os.path.isdir(SETTINGS_DIR):  # pragma: no cover
 CONFIG_FILE = os.path.join(SETTINGS_DIR, CONFIG_FILE_NAME)
 
 
-def _get_installed_windows_versions(supported_versions=SUPPORTED_ANSYS_VERSIONS):
+def _get_installed_windows_versions(
+    supported_versions: SUPPORTED_VERSIONS_TYPE = SUPPORTED_ANSYS_VERSIONS,
+):  # pragma: no cover
     """Get the AWP_ROOT environment variable values for supported versions."""
 
     # The student version overwrites the AWP_ROOT env var
@@ -103,7 +105,7 @@ def _get_default_linux_base_path():
     return None  # pragma: no cover
 
 
-def _get_default_windows_base_path():
+def _get_default_windows_base_path():  # pragma: no cover
     """Get the default base path of the Ansys unified install on windows."""
 
     base_path = os.path.join(os.environ["PROGRAMFILES"], "ANSYS INC")
@@ -265,7 +267,7 @@ def find_mechanical(version=None, supported_versions=SUPPORTED_ANSYS_VERSIONS):
     ans_path, version = _get_unified_install_base_for_version(version, supported_versions)
     if not ans_path or not version:
         return "", ""
-    if is_windows():
+    if is_windows():  # pragma: no cover
         mechanical_bin = os.path.join(ans_path, "aisol", "bin", "winx64", f"AnsysWBU.exe")
     else:
         mechanical_bin = os.path.join(ans_path, "aisol", ".workbench")
@@ -345,7 +347,7 @@ def is_valid_executable_path(product: str, exe_loc: str):
             and re.search(r"ansys\d\d\d", os.path.basename(os.path.normpath(exe_loc))) is not None
         )
     elif product == "mechanical":
-        if is_windows():
+        if is_windows():  # pragma: no cover
             return (
                 os.path.isfile(exe_loc)
                 and re.search("AnsysWBU.exe", os.path.basename(os.path.normpath(exe_loc)))
@@ -362,42 +364,38 @@ def _is_common_executable_path(product: str, exe_loc: str) -> bool:
     if product == "mapdl":
         path = os.path.normpath(exe_loc)
         path = path.split(os.sep)
-        if (
-            re.search(r"v(\d\d\d)", exe_loc) is not None
-            and re.search(r"ansys(\d\d\d)", exe_loc) is not None
-        ):
-            equal_version = (
-                re.search(r"v(\d\d\d)", exe_loc)[1] == re.search(r"ansys(\d\d\d)", exe_loc)[1]
-            )
-        else:
-            equal_version = False
-
+        # Look for all v(\d\d\d) to catch the last one
+        # in case the user has placed the installation folder inside a folder called for example (/ansys/v211)
+        v_version = re.findall(r"v(\d\d\d)", exe_loc)
+        ansys_version = re.findall(r"ansys(\d\d\d)", exe_loc, re.IGNORECASE)
         return (
-            is_valid_executable_path("mapdl", exe_loc)
-            and re.search(r"v\d\d\d", exe_loc)
+            len(v_version) != 0
+            and len(ansys_version) != 0
+            and v_version[-1] == ansys_version[-1]
+            and is_valid_executable_path("mapdl", exe_loc)
             and "ansys" in path
             and "bin" in path
-            and equal_version
         )
+
     elif product == "mechanical":
         path = os.path.normpath(exe_loc)
         path = path.split(os.sep)
 
-        is_valid_path = is_valid_executable_path(exe_loc)
+        is_valid_path = is_valid_executable_path("mechanical", exe_loc)
 
-        if is_windows():
+        if is_windows():  # pragma: no cover
             return (
                 is_valid_path
-                and re.search(r"v\d\d\d", exe_loc)
+                and re.search(r"v\d\d\d", exe_loc) is not None
                 and "aisol" in path
-                and "bin" in path
+                and ("bin" in path or "Bin" in path)
                 and "winx64" in path
-                and "AnsysWBU.exe" in path
+                and ("AnsysWBU.exe" in path or "ANSYSWBU.exe" in path)
             )
 
         return (
             is_valid_path
-            and re.search(r"v\d\d\d", exe_loc)
+            and re.search(r"v\d\d\d", exe_loc) is not None
             and "aisol" in path
             and ".workbench" in path
         )
@@ -489,9 +487,8 @@ def _save_path(product: str, exe_loc: str = None, allow_prompt=True) -> str:
         _change_default_path(product, exe_loc)
         return exe_loc
 
-    if exe_loc is not None:
-        if is_valid_executable_path(product, exe_loc):
-            return exe_loc
+    if is_valid_executable_path(product, exe_loc):
+        return exe_loc
     if allow_prompt:
         exe_loc = _prompt_path(product)
     return exe_loc
