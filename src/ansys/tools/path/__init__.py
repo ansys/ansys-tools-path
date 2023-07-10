10686e381fe41387c43ce710c8274f3785a8af80
Gryfenfer97 <hajjarjamil4@gmail.com>
2023-07-10 18:41:23 +0200

    expose functions in __init__.py

--- src/ansys/tools/path/__init__.py
+++ src/ansys/tools/path/__init__.py
@@ -28,3 +28,23 @@ from ansys.tools.path.path import change_default_ansys_path  # deprecated
 from ansys.tools.path.path import find_ansys  # deprecated
 from ansys.tools.path.path import get_ansys_path  # deprecated
 from ansys.tools.path.path import save_ansys_path  # deprecated
+
+__all__ = [
+    "SETTINGS_DIR",
+    "SUPPORTED_ANSYS_VERSIONS",
+    "change_default_mapdl_path",
+    "change_default_mechanical_path",
+    "find_mapdl",
+    "find_mechanical",
+    "get_available_ansys_installations",
+    "get_latest_ansys_installation",
+    "get_mapdl_path",
+    "get_mechanical_path",
+    "save_mapdl_path",
+    "save_mechanical_path",
+    "version_from_path",
+    "change_default_ansys_path",
+    "find_ansys",
+    "get_ansys_path",
+    "save_ansys_path",
+]
