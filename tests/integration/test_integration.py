e72a26242771c9c71dd4f3230016579825f4a6c9
Mohamed Koubaa <koubaa@github.com>
2023-10-19 07:45:54 -0500

    no dyna in mapdl docker file

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -5,10 +5,8 @@ import pytest
 
 from ansys.tools.path import (
     clear_configuration,
-    find_dyna,
     find_mapdl,
     get_available_ansys_installations,
-    save_dyna_path,
     save_mapdl_path,
 )
 from ansys.tools.path.path import CONFIG_FILE
@@ -21,13 +19,6 @@ skip_if_not_ansys_local = pytest.mark.skipif(
 @skip_if_not_ansys_local
 def test_find_mapdl():
     bin_file, ver = find_mapdl()
-    assert os.path.isfile(bin_file)
-    assert ver != ""
-
-
-@skip_if_not_ansys_local
-def test_find_dyna():
-    bin_file, ver = find_dyna()
     assert os.path.isfile(bin_file)
     assert ver != ""
 
@@ -60,26 +51,3 @@ def test_save_mapdl_path():
         with open(CONFIG_FILE, "w") as config_file:
             config_file.write(old_config)
 
-
-@skip_if_not_ansys_local
-@pytest.mark.linux
-def test_save_dyna_path():
-    if not os.path.isfile(CONFIG_FILE):
-        old_config = None
-    else:
-        with open(CONFIG_FILE, "r") as config_file:
-            old_config = config_file.read()
-
-    path, _ = find_dyna(version=222)
-
-    assert save_dyna_path(path, allow_prompt=False)
-    with open(CONFIG_FILE, "r") as config_file:
-        assert json.loads(config_file.read()) == {"dyna": "/ansys_inc/v222/ansys/bin/lsdyna222"}
-
-    assert save_dyna_path(None, allow_prompt=False)
-    with open(CONFIG_FILE, "r") as config_file:
-        assert json.loads(config_file.read()) == {"dyna": "/ansys_inc/v222/ansys/bin/lsdyna222"}
-    clear_configuration("all")
-    if old_config is not None:
-        with open(CONFIG_FILE, "w") as config_file:
-            config_file.write(old_config)
